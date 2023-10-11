7be3d967463371bbb15e2e617e22ac0243fad6f0
Miguel Bomfim <miguelaubom@gmail.com>
2023-10-11 02:47:00 -0300

    Some improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,22 +10,32 @@ export default function Header({ pathname }: { pathname: string }) {
   return (
     <div
       className={clsx('grid h-screen content-center justify-center p-8', {
-        'grid-cols-0': pathname === '/',
+        'grid-cols-0': pathname === ('/' || '/br'),
       })}
     >
-      <p className="font-mono text-[1.5vw] uppercase leading-none">{t('hi')}</p>
+      <p className="font-mono text-[1.5vw] uppercase leading-none antialiased">
+        {t('hi')}
+      </p>
       <div className="flex flex-col justify-self-center">
         <div className="flex items-center justify-between">
-          <p className="grow text-[7vw] font-bold uppercase leading-none">
+          <p
+            className={clsx(
+              'grow font-bold uppercase leading-none antialiased',
+              {
+                'text-[4vw]': pathname === ('/' || '/br'),
+                'text-[7vw]': pathname === '/en',
+              },
+            )}
+          >
             {t('jobTitle').split(' ')[0]}
           </p>
           <Social pathname={pathname} />
         </div>
-        <p className="text-[10vw] font-bold uppercase leading-none">
+        <p className=" text-[10vw] font-bold uppercase leading-none antialiased">
           {t('jobTitle').split(' ')[1]}
         </p>
       </div>
-      <p className="w-2/3 justify-self-end text-end font-mono text-[1.5vw] uppercase leading-none">
+      <p className=" w-2/3 justify-self-end text-end font-mono text-[1.5vw] uppercase leading-none antialiased">
         {t('bye')}
       </p>
     </div>
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,21 +1,19 @@
-import {
-  FaLinkedin as LinkedIn,
-  FaGithubSquare as GitHub,
-} from 'react-icons/fa'
+import { FaLinkedin as LinkedIn, FaGithub as GitHub } from 'react-icons/fa'
 import { SiGoogledocs as Doc } from 'react-icons/si'
 
 import React from 'react'
 
 export default function Social({ pathname }: { pathname: string }) {
+  // fill-[#0a66c2]
   return (
-    <ul className="flex grow justify-evenly">
+    <ul className="flex grow gap-2">
       <li>
         <a
           target="_blank"
           href="https://linkedin.com/in/miguel-bomfim"
           rel="noreferrer"
         >
-          <LinkedIn className="bg-white fill-[#0a66c2] text-8xl transition delay-75 hover:scale-105" />
+          <LinkedIn className=" fill-[#a4c2e0] text-[3.5vw] transition delay-75 ease-linear hover:scale-110 hover:fill-[#0a66c2]" />
         </a>
       </li>
       <li>
@@ -24,7 +22,7 @@ export default function Social({ pathname }: { pathname: string }) {
           href="https://github.com/miguel-bomfim"
           rel="noreferrer"
         >
-          <GitHub className="text-8xl transition delay-75 hover:scale-105" />
+          <GitHub className="h-auto fill-gray-200 text-[3.5vw] transition delay-75 ease-linear hover:scale-110 hover:fill-gray-800 " />
         </a>
       </li>
       <li>
@@ -38,7 +36,7 @@ export default function Social({ pathname }: { pathname: string }) {
             pathname === ('/' || '/br') ? 'Currículo Miguel' : 'CV Miguel'
           }
         >
-          <Doc className="delay-50 text-8xl transition hover:scale-105" />
+          <Doc className="delay-50 fill-gray-300 text-[3.5vw] transition ease-linear hover:scale-110 hover:fill-black" />
         </a>
       </li>
     </ul>
