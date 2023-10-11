6ec667d050314e7ba751156b6d6a8e86d6dc702c
Miguel Bomfim <miguelaubom@gmail.com>
2023-10-11 00:45:51 -0300

    Update default locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,17 @@
 import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
-  locales: ['br', 'en'],
   defaultLocale: 'br',
+  locales: ['br', 'en'],
 })
 
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  // Matcher entries are linked with a logical "or", therefore
+  // if one of them matches, the middleware will be invoked.
+  matcher: [
+    // Match all pathnames without `.`
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+    // Match all pathnames within `/users`, optionally with a locale prefix
+    '/(.+)?/users/(.+)',
+  ],
 }
