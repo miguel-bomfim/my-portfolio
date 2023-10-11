05df63f6d92c2b3ff70067226706f623d5a9e148
Miguel Bomfim <miguel@MacBook-Pro-de-Sarah.local>
2023-10-11 18:03:38 -0300

    Fix useMobile hook

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react'
 
 const useMobile = () => {
-  const [width, setWidth] = useState<number>(window.innerWidth)
-
-  const handleWindowSizeChange = () => {
-    setWidth(window.innerWidth)
+  const checkMobile = () => {
+    if (typeof window !== 'undefined') {
+      // Check the screen width to determine if it's a mobile device
+      return window.innerWidth < 768 // You can adjust the threshold as needed
+    }
+    return false
   }
 
+  const [mobile, setMobile] = useState(false)
+
   useEffect(() => {
-    window.addEventListener('resize', handleWindowSizeChange)
-    return () => {
-      window.removeEventListener('resize', handleWindowSizeChange)
+    if (typeof window !== 'undefined') {
+      setMobile(checkMobile())
+
+      // Add a listener to update the 'mobile' state if the window is resized
+      const handleResize = () => {
+        setMobile(checkMobile())
+      }
+
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        // Clean up the event listener when the component is unmounted
+        window.removeEventListener('resize', handleResize)
+      }
     }
   }, [])
 
-  const isMobile = width <= 768
-
-  return isMobile
+  return mobile
 }
 
 export default useMobile
