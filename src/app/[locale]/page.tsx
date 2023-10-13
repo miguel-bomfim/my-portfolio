05c7ba1ab6e1888af635143d0b87453d19254ae4
Miguel Bomfim <miguel@MBP-de-Sarah.HotSpot300>
2023-10-13 17:08:19 -0300

    Add framer-motion package

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@
 import Header from '@/components/Header'
 import IntlButtons from '@/components/IntlButtons'
 import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
 
 export default function Index() {
   const pathname = usePathname()
@@ -10,9 +11,18 @@ export default function Index() {
   return (
     <>
       <IntlButtons pathname={pathname} />
-      <div className="flex justify-center">
+      <motion.div
+        className="flex justify-center"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.3,
+          delay: 0.2,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
         <Header pathname={pathname} />
-      </div>
+      </motion.div>
     </>
   )
 }
