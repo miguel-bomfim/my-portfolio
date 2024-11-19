0b1764a0c3e2bcfe906431fa95788ab81aa714a9
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2024-11-19 17:58:45 -0300

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,32 +2,53 @@ import React from 'react'
 import styles from './styles/Carousel.module.css'
 import { FaReact, FaNodeJs, FaGit, FaDocker } from 'react-icons/fa'
 import { SiTailwindcss, SiPrisma } from 'react-icons/si'
-
-const images = [
-  <FaReact key={1} size={48} color="white" />,
-  <FaNodeJs key={2} size={48} color="white" />,
-  <SiTailwindcss key={3} size={48} color="white" />,
-  <FaGit key={3} size={48} color="white" />,
-  <FaDocker key={3} size={48} color="white" />,
-  <SiPrisma key={3} size={48} color="white" />,
-]
+import useMobile from "@/hooks/useIsMobile";
 
 const Carousel: React.FC = () => {
+  const isMobile = useMobile();
+
+  const techs = [
+    {
+      name: 'React',
+      icon: <FaReact key={1} size={isMobile ? 36 : 48} color='white' />,
+    },
+    {
+      name: 'NodeJs',
+      icon: <FaNodeJs key={2} size={isMobile ? 36 : 48} color='white' />,
+    },
+    {
+      name: 'Tailwindcss',
+      icon: <SiTailwindcss key={3} size={isMobile ? 36 : 48} color='white' />,
+    },
+    {
+      name: 'Git',
+      icon: <FaGit key={3} size={isMobile ? 36 : 48} color='white' />,
+    },
+    {
+      name: 'Docker',
+      icon: <FaDocker key={3} size={isMobile ? 36 : 48} color='white' />,
+    },
+    {
+      name: 'Prisma',
+      icon: <SiPrisma key={3} size={isMobile ? 36 : 48} color='white' />,
+    },
+  ];
+  
   return (
-    <div className={styles.carousel}>
+    <div className={`absolute bottom-0 ${styles.carousel}`}>
       <div className={styles.group}>
-        {images.map((src, index) => (
+        {techs.map((tech, index) => (
           <div className={`${styles.icon} ${styles.tooltip}`} key={index}>
-            {src}
-            <span className={styles.tooltiptext}>Tooltip text</span>
+            {tech.icon}
+            <span className={styles.tooltiptext}>{tech.name}</span>
           </div>
         ))}
       </div>
       <div aria-hidden className={styles.group}>
-        {images.map((src, index) => (
+        {techs.map((tech, index) => (
           <div className={`${styles.icon} ${styles.tooltip}`} key={index}>
-            {src}
-            <span className={styles.tooltiptext}>Tooltip text</span>
+            {tech.icon}
+            <span className={styles.tooltiptext}>{tech.name}</span>
           </div>
         ))}
       </div>
