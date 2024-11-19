8273207e7bff9b34c8d1c0aeeb1209ef84457e71
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2024-11-19 18:03:17 -0300

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,38 +2,37 @@ import React from 'react'
 import styles from './styles/Carousel.module.css'
 import { FaReact, FaNodeJs, FaGit, FaDocker } from 'react-icons/fa'
 import { SiTailwindcss, SiPrisma } from 'react-icons/si'
-import useMobile from "@/hooks/useIsMobile";
+import useMobile from '@/hooks/useIsMobile'
 
 const Carousel: React.FC = () => {
-  const isMobile = useMobile();
+  const isMobile = useMobile()
 
   const techs = [
     {
       name: 'React',
-      icon: <FaReact key={1} size={isMobile ? 36 : 48} color='white' />,
+      icon: <FaReact key={1} size={isMobile ? 36 : 48} color="white" />,
     },
     {
       name: 'NodeJs',
-      icon: <FaNodeJs key={2} size={isMobile ? 36 : 48} color='white' />,
+      icon: <FaNodeJs key={2} size={isMobile ? 36 : 48} color="white" />,
     },
     {
       name: 'Tailwindcss',
-      icon: <SiTailwindcss key={3} size={isMobile ? 36 : 48} color='white' />,
+      icon: <SiTailwindcss key={3} size={isMobile ? 36 : 48} color="white" />,
     },
     {
       name: 'Git',
-      icon: <FaGit key={3} size={isMobile ? 36 : 48} color='white' />,
+      icon: <FaGit key={3} size={isMobile ? 36 : 48} color="white" />,
     },
     {
       name: 'Docker',
-      icon: <FaDocker key={3} size={isMobile ? 36 : 48} color='white' />,
+      icon: <FaDocker key={3} size={isMobile ? 36 : 48} color="white" />,
     },
     {
       name: 'Prisma',
-      icon: <SiPrisma key={3} size={isMobile ? 36 : 48} color='white' />,
+      icon: <SiPrisma key={3} size={isMobile ? 36 : 48} color="white" />,
     },
-  ];
-  
+  ]
   return (
     <div className={`absolute bottom-0 ${styles.carousel}`}>
       <div className={styles.group}>
