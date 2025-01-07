f853b6b7c1183288af8151dbd436ad1fb5757e05
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-01-07 17:11:09 -0300

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,40 +1,40 @@
-"use client";
+'use client'
 
-import React from "react";
-import styles from "./styles/Carousel.module.css";
-import { FaReact, FaNodeJs, FaGit, FaDocker } from "react-icons/fa";
-import { SiTailwindcss, SiPrisma } from "react-icons/si";
-import useMobile from "@/hooks/useIsMobile";
+import React from 'react'
+import styles from './styles/Carousel.module.css'
+import { FaReact, FaNodeJs, FaGit, FaDocker } from 'react-icons/fa'
+import { SiTailwindcss, SiPrisma } from 'react-icons/si'
+import useMobile from '@/hooks/useIsMobile'
 
 const Carousel: React.FC = () => {
-  const isMobile = useMobile();
+  const isMobile = useMobile()
 
   const techs = [
     {
-      name: "React",
-      icon: <FaReact key={1} size={isMobile ? 36 : 48} color="white" />,
+      name: 'React',
+      icon: <FaReact key={1} size={isMobile ? 36 : 48} color='white' />,
     },
     {
-      name: "NodeJs",
-      icon: <FaNodeJs key={2} size={isMobile ? 36 : 48} color="white" />,
+      name: 'NodeJs',
+      icon: <FaNodeJs key={2} size={isMobile ? 36 : 48} color='white' />,
     },
     {
-      name: "Tailwindcss",
-      icon: <SiTailwindcss key={3} size={isMobile ? 36 : 48} color="white" />,
+      name: 'Tailwindcss',
+      icon: <SiTailwindcss key={3} size={isMobile ? 36 : 48} color='white' />,
     },
     {
-      name: "Git",
-      icon: <FaGit key={3} size={isMobile ? 36 : 48} color="white" />,
+      name: 'Git',
+      icon: <FaGit key={3} size={isMobile ? 36 : 48} color='white' />,
     },
     {
-      name: "Docker",
-      icon: <FaDocker key={3} size={isMobile ? 36 : 48} color="white" />,
+      name: 'Docker',
+      icon: <FaDocker key={3} size={isMobile ? 36 : 48} color='white' />,
     },
     {
-      name: "Prisma",
-      icon: <SiPrisma key={3} size={isMobile ? 36 : 48} color="white" />,
+      name: 'Prisma',
+      icon: <SiPrisma key={3} size={isMobile ? 36 : 48} color='white' />,
     },
-  ];
+  ]
   return (
     <div className={`absolute bottom-0 ${styles.carousel}`}>
       <div className={styles.group}>
