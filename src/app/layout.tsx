0022b90f98bd972a18f1ba21fff7c0c7951b9fa2
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-08-20 23:53:36 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,25 +10,16 @@ export default function RootLayout({
   return (
     <html>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9534755771299362" />
+        
         <Script
           strategy="afterInteractive"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9534755771299362"
           crossOrigin="anonymous"
         />
-        <Script id="adsbygoogle-init" strategy="afterInteractive">
-          {`(adsbygoogle = window.adsbygoogle || []).push({});`}
-        </Script>
       </head>
-      <body>
-        {children}
-        <ins className="adsbygoogle"
-             style={{ display: 'block' }}
-             data-ad-client="ca-pub-9534755771299362"
-             data-ad-slot="1234567890"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
