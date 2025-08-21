53604fa2645fc0e3b70462ef858f36908f4c13e7
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-08-20 23:50:09 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Script from 'next/script'
+import React from 'react'
 
 export default function RootLayout({
   children,
@@ -8,15 +9,26 @@ export default function RootLayout({
 }) {
   return (
     <html>
-        <head>
-            <Script
-                strategy="afterInteractive"
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9534755771299362"
-                crossOrigin="anonymous"
-            ></Script>
-        </head>
-        <body>{children}</body>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9534755771299362"
+          crossOrigin="anonymous"
+        />
+        <Script id="adsbygoogle-init" strategy="afterInteractive">
+          {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+        </Script>
+      </head>
+      <body>
+        {children}
+        <ins className="adsbygoogle"
+             style={{ display: 'block' }}
+             data-ad-client="ca-pub-9534755771299362"
+             data-ad-slot="1234567890"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+      </body>
     </html>
   )
 }
