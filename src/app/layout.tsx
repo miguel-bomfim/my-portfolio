8801d0469466aa9b2cef2d64c39f20b6ec67ad08
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-08-20 23:32:57 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,14 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>{children}</body>
+        <head>
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9534755771299362"
+                crossorigin="anonymous"
+            ></Script>
+        </head>
+        <body>{children}</body>
     </html>
   )
 }
